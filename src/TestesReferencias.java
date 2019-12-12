924365280291a2f3ead3f1e9ba75056735bcb22c
marciofmpq <marciofelipequeiroga@gmail.com>
2019-12-12 14:47:27 -0300

    Add EditorVideo e ControleBonificacao com Polimorfismo

--- src/TestesReferencias.java
+++ src/TestesReferencias.java
@@ -13,10 +13,15 @@ public class TestesReferencias {
         e1.setNome("Joca");
         e1.setSalario(2500.00);
 
+        Designer d = new Designer();
+        e1.setNome("Julia");
+        e1.setSalario(2000.00);
+
         ControleBonificacao controle =  new ControleBonificacao();
         controle.registra(g1);
         controle.registra(f1);
         controle.registra(e1);
+        controle.registra(d);
 
         System.out.println(controle.getSoma());
 
